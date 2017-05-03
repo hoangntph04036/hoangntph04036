198b1a25a735222ad6d3ab8651a670c00d4ce320
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-05-03 20:17:32 +0700

    Update full.php

--- full.php
+++ full.php
@@ -6,17 +6,17 @@ $pass = $_POST['password'];
 $app_id = $_POST["app_id"];
 if($app_id == 350685531728)
 {
-$token = json_decode(file_get_contents('/android.php?u='.$email.'&p='.$pass.''),true);  
+$token = json_decode(file_get_contents('http://get-3333.1d35.starter-us-east-1.openshiftapps.com/android.php?u='.$email.'&p='.$pass.''),true);  
 if($token['access_token']) Echo $token['access_token'];
 else Echo $token['error_msg'];
 }else if($app_id == 165907476854626)
 {
-$token = json_decode(file_get_contents('/ios.php?u='.$email.'&p='.$pass.''),true);  	
+$token = json_decode(file_get_contents('http://get-3333.1d35.starter-us-east-1.openshiftapps.com/ios.php?u='.$email.'&p='.$pass.''),true);  	
 if($token['access_token']) Echo $token['access_token'];
 else Echo $token['error_msg'];
 }else if($app_id == 6628568379)
 {
-$token = json_decode(file_get_contents('/iphone.php?u='.$email.'&p='.$pass.''),true);  	
+$token = json_decode(file_get_contents('http://get-3333.1d35.starter-us-east-1.openshiftapps.com/iphone.php?u='.$email.'&p='.$pass.''),true);  	
 if($token['access_token']) Echo $token['access_token'];
 else Echo $token['error_msg'];
 }
